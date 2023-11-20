0bf87e166f278cfa09e077965afecca27eb09089
Salomé Gallego A <sgallegoa@unitecnica.net>
2023-11-20 09:24:38 -0500

    Estado para almacenar los productos

--- test/src/components/ProductSelector.tsx
+++ test/src/components/ProductSelector.tsx
@@ -3,5 +3,8 @@ import React, { useState, useEffect } from 'react';
 interface ProductSelectorProps {
     onProductSelect: (product: string) => void;
 }
+const ProductSelector: React.FC<ProductSelectorProps> = ({ onProductSelect }) => {
+    // Estado para almacenar la lista de productos
+    const [products, setProducts] = useState<string[]>([]);
 
 export default ProductSelector;
