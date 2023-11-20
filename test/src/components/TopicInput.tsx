774d3476c48f88fc29e68b4590563fee7a87f2b2
Salomé Gallego A <sgallegoa@unitecnica.net>
2023-11-20 09:50:12 -0500

    Corrigiendo API

--- test/src/components/TopicInput.tsx
+++ test/src/components/TopicInput.tsx
@@ -1,30 +1,32 @@
 import React, { useState } from 'react';
 
 interface TopicInputProps {
-onChange: (value: string) => void;
+  onChange: (value: string) => void;
 }
 
-onst TopicInput: React.FC<TopicInputProps> = ({ onChange }) => {
-    // Estado para almacenar el valor del input
-    const [topic, setTopic] = useState('');
+const TopicInput: React.FC<TopicInputProps> = ({ onChange }) => {
+  // Estado para almacenar el valor del input
+  const [topic, setTopic] = useState('');
 
-    // Manejar cambios en el input y llamar a la función onChange
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  // Manejar cambios en el input y llamar a la función onChange
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTopic(e.target.value);
     onChange(e.target.value);
-    return (
-        <div className="mb-4">
-          {/* Label del input */}
-        <label className="block text-gray-700 text-sm font-bold mb-2">Topic:</label>
-          {/* Input del Topic */}
-        <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            value={topic}
-            onChange={handleInputChange}
-        />
-        </div>
-    );
-    };
+  };
 
-    export default TopicInput;
+  return (
+    <div className="mb-4">
+      {/* Label del input */}
+      <label className="block text-gray-700 text-sm font-bold mb-2">Topic:</label>
+      {/* Input del Topic */}
+      <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        type="text"
+        value={topic}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+};
+
+export default TopicInput;
