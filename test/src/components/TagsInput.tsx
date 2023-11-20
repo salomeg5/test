fad426681826b536c4db0432ee61041ce5425d7c
Salomé Gallego A <sgallegoa@unitecnica.net>
2023-11-20 09:19:08 -0500

    Manejando los cambios en el input

--- test/src/components/TagsInput.tsx
+++ test/src/components/TagsInput.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 
 interface TagsInputProps {
-  onChange: (tags: string[]) => void;
+onChange: (tags: string[]) => void;
 }
 
 const TagsInput: React.FC<TagsInputProps> = ({ onChange }) => {
   // Estado para almacenar las tags
-  const [tags, setTags] = useState<string[]>([]);
+const [tags, setTags] = useState<string[]>([]);
   // Estado para almacenar el valor del input de tags
-  const [tagInput, setTagInput] = useState('');
-  export default TagsInput;
+const [tagInput, setTagInput] = useState('');
+   // Manejar cambios en el input de tags
+const handleTagInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTagInput(e.target.value);
+};
+
+
+export default TagsInput;
