e77116413693cc92ab1c111cc3f7fa59f978d265
Salomé Gallego A <sgallegoa@unitecnica.net>
2023-11-20 09:35:45 -0500

    Estilizando

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -23,3 +23,14 @@ const App: React.FC = () => {
  const handleProductSelect = (product: string) => {
   setSelectedProduct(product);
 };
+return (
+  <div className="max-w-2xl mx-auto p-4"> {/* Contenedor principal con margen y padding */}
+    <h1 className="text-3xl font-bold mb-4">Merchant Information Input Page</h1> {/* Título grande y negrita */}
+
+    <div className="mb-4"> {/* Espaciado inferior para los componentes */}
+      <TopicInput onChange={handleTopicChange} /> {/* Componente de TopicInput */}
+    </div>
+
+    <div className="mb-4"> {/* Espaciado inferior para los componentes */}
+      <TagsInput onChange={handleTagsChange} /> {/* Componente de TagsInput */}
+    </div>
