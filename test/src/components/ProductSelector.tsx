0c71be215481a0b320f2da14b021c053cf497bb9
Salomé Gallego A <sgallegoa@unitecnica.net>
2023-11-20 09:26:30 -0500

    Opciones de la API y estilizando

--- test/src/components/ProductSelector.tsx
+++ test/src/components/ProductSelector.tsx
@@ -13,4 +13,22 @@ const ProductSelector: React.FC<ProductSelectorProps> = ({ onProductSelect }) =>
         const selectedProduct = e.target.value;
         onProductSelect(selectedProduct);
       };
+      return (
+        <div className="mb-4">
+          {/* Label del selector de productos */}
+          <label className="block text-gray-700 text-sm font-bold mb-2">Products:</label>
+          {/* Selector de productos */}
+          <select
+            className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            onChange={handleProductSelect}
+          >
+            <option value="" disabled>Select a product</option>
+            {/* Opciones de productos obtenidas de la API */}
+            {products.map((product, index) => (
+            <option key={index} value={product}>{product}</option>
+            ))}
+        </select>
+        </div>
+    );
+    };
 export default ProductSelector;
