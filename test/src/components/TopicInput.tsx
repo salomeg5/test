462003597957b47f752d98d93fff82a8ce24b752
Salomé Gallego A <sgallegoa@unitecnica.net>
2023-11-20 09:13:57 -0500

    Estado para almacenar y manejar los cambios

--- test/src/components/TopicInput.tsx
+++ test/src/components/TopicInput.tsx
@@ -3,3 +3,13 @@ import React, { useState } from 'react';
 interface TopicInputProps {
   onChange: (value: string) => void;
 }
+
+onst TopicInput: React.FC<TopicInputProps> = ({ onChange }) => {
+    // Estado para almacenar el valor del input
+    const [topic, setTopic] = useState('');
+  
+    // Manejar cambios en el input y llamar a la función onChange
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setTopic(e.target.value);
+      onChange(e.target.value);
+      
