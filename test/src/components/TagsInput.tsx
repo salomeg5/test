3d104c460ae3d930c003be8042c34db7f6b3a533
Salomé Gallego A <sgallegoa@unitecnica.net>
2023-11-20 09:18:04 -0500

    Estado para almacenar las tags y el valor

---
+++ test/src/components/TagsInput.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+interface TagsInputProps {
+  onChange: (tags: string[]) => void;
+}
+
+const TagsInput: React.FC<TagsInputProps> = ({ onChange }) => {
+  // Estado para almacenar las tags
+  const [tags, setTags] = useState<string[]>([]);
+  // Estado para almacenar el valor del input de tags
+  const [tagInput, setTagInput] = useState('');
+  export default TagsInput;
