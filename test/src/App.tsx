bb2aa8b4e11853e4819256b626abc0ae99666932
Salomé Gallego A <sgallegoa@unitecnica.net>
2023-11-20 09:32:34 -0500

    Importando los componentes

---
+++ test/src/App.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import TopicInput from './components/TopicInput';
+import TagsInput from './components/TagsInput';
+import ProductSelector from './components/ProductSelector';
