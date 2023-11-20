addefdbeefaaaf778175f2f85db2b8c077523825
Salomé Gallego A <sgallegoa@unitecnica.net>
2023-11-20 09:33:41 -0500

    Estado para almacenar la info

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -2,3 +2,9 @@ import React from 'react';
 import TopicInput from './components/TopicInput';
 import TagsInput from './components/TagsInput';
 import ProductSelector from './components/ProductSelector';
+
+const App: React.FC = () => {
+   // Estados para almacenar la información ingresada por el usuario
+  const [topic, setTopic] = React.useState('');
+  const [tags, setTags] = React.useState<string[]>([]);
+  const [selectedProduct, setSelectedProduct] = React.useState('');
